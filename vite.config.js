fddbc28a61e589c6b5c7935d190ea99a489cc37f
stteenArts <stteenriwi@gmail.com>
2026-06-02 14:05:08 -0500

    fixed: css vite config no path of styles

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,15 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/dashboard.css',
+                'resources/css/login.css',
+                'resources/css/paymentEvent.css',
+                'resources/css/showEvent.css',
+                'resources/css/welcome.css', 
+                'resources/js/app.js',
+            ],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
